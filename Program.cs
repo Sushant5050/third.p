b2bf7bea6ad2ef9d76f52a4fac2c05b8c2ceaea8
Sushant5050 <148970856+Sushant5050@users.noreply.github.com>
2023-12-01 14:55:46 +0530

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace third.p
+{
+    internal class Program
+    {
+        public void Division()
+        {
+            int a, b, c;
+            a = 10; b = 5; c = a / b;
+            Console.WriteLine("Division=" + c);
+
+        }
+
+
+
+        static void Main(string[] args)
+        {
+            Program p = new Program();
+            p.Division();
+            Console.ReadLine();
+        }
+        
+    }
+}
